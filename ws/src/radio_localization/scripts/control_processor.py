c5d8350d8f4ff3eaa0889707acf82ec0e8be5519
Bryan Yaggi <yaggi@tamu.edu>
2025-04-21 12:14:12 -0500

    Set up measurement and control processors
    - Add vehicle params to small scenario config

--- ws/src/radio_localization/scripts/control_processor.py
+++ ws/src/radio_localization/scripts/control_processor.py
@@ -2,7 +2,8 @@
 
 import rospy
 import tf
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import TwistStamped
+from dbw_fca_msgs.msg import SteeringReport
 from std_msgs.msg import Float64MultiArray
 
 from radio_localization.moving_averager import MovingAverager
@@ -18,25 +19,41 @@ class ControlProcessor:
         velocityBuffer = int(period * rates['velocity_raw'])
         steerBuffer = int(period * rates['steer_raw'])
         publishFrequency = rates['control_processed']
-        
-        self.msg = Float64MultiArray()
 
+        # Get steering ratio
+        if not rospy.has_param('vehicle'):
+            rospy.logerr("Parameter 'vehicle' not found.")
+            return
+        vehicle = rospy.get_param('vehicle')
+        self.steeringRatio = vehicle['steering_ratio']
+        
+        # Initialize message
+        self.msg = Float64MultiArray()
+        self.msg.data = [0.0] * 2
+
+        # Set up moving averagers
         self.velocityMovingAverager = MovingAverager(buffer=velocityBuffer)
         self.steerMovingAverager = MovingAverager(buffer=steerBuffer)
 
         rospy.Subscriber('vehicle/twist', TwistStamped, self.velocityCallback)
-        rospy.subscriber('vehicle/steering_report', SteeringReport, self.steerCallback)
+        rospy.Subscriber('vehicle/steering_report', SteeringReport, self.steerCallback)
         self.pub = rospy.Publisher('control', Float64MultiArray, queue_size=1)
         rospy.Timer(rospy.Duration(1 / publishFrequency), self.timerCallback)
 
     def velocityCallback(self, msg):
-        self.movingAverager.append(msg.twist.)
+        self.velocityMovingAverager.append(msg.twist.linear.x)
+
+    def steerCallback(self, msg):
+        steeringWheelAngle = msg.steering_wheel_angle
+        roadWheelAngle = steeringWheelAngle / self.steeringRatio
+        self.steerMovingAverager.append(roadWheelAngle)
 
     def timerCallback(self, event):
-        self.msg.data = self.movingAverager.getAverage()
+        self.msg.data[0] = self.velocityMovingAverager.getAverage()
+        self.msg.data[1] = self.steerMovingAverager.getAverage()
         self.pub.publish(self.msg)
 
 if __name__ == '__main__':
     rospy.init_node('yaw_measurement_processor')
-    yp = YawMeasurementProcessor()
+    ControlProcessor()
     rospy.spin()
