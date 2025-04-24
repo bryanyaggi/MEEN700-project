a63f7b2390cfb3cc272c8bfa9e3a3c58f9cfaadb
matthew <matthewhancock@tamu.edu>
2025-04-23 22:31:39 -0500

    Ease of use changes to parse_data_mapFrame.py

--- parse_data_mapFrame.py
+++ parse_data_mapFrame.py
@@ -1,6 +1,18 @@
 from bagpy import bagreader
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+os.system('cls')
+
+scenario = 'Large'
+
+if scenario == 'Large':
+    bagfile = r"C:\Users\hanco\OneDrive\Documents\Texas A&M\MS Mechanical Engineering\2025 Spring\MEEN 700\Project\data\radio-localization_2025-04-18-03-08-11.bag" # large_scenario
+    yamlfile = 'large_scenario_basestations.yaml'
+elif scenario == 'Small':
+    bagfile = r"C:\Users\hanco\OneDrive\Documents\Texas A&M\MS Mechanical Engineering\2025 Spring\MEEN 700\Project\data\radio-localization_2025-04-18-02-25-19.bag" # small_scenario
+    yamlfile = 'small_scenario_basestations.yaml'
 
 import numpy as np
 from scipy.optimize import curve_fit
@@ -32,14 +44,20 @@ def getBaseStationPoints(yamlFile):
 
     return config_data, base_stations
 
+def MSE(yhat, ydata):
+    if len(yhat) != len(ydata):
+        raise Warning('yhat and ydata must be of the same length')
+    return np.sum((ydata - yhat)**2)/len(yhat)
+
 print("Getting base station positions...")
-config_data, baseStations = getBaseStationPoints('large_scenario_basestations.yaml')
+# config_data, baseStations = getBaseStationPoints('large_scenario_basestations.yaml')
+config_data, baseStations = getBaseStationPoints(yamlfile)
 
 # bagfile = 'radio-localization_2025-04-17-00-14-14.bag'
 # bagfile = 'radio-localization_2025-04-16-23-35-41.bag'
 # bagfile = 'radio-localization_2025-04-17-00-14-14.bag'
 # bagfile = r"C:\Users\hanco\OneDrive\Documents\Texas A&M\MS Mechanical Engineering\2025 Spring\MEEN 700\Project\data\radio-localization_2025-04-18-02-25-19.bag" # small_scenario
-bagfile = r"C:\Users\hanco\OneDrive\Documents\Texas A&M\MS Mechanical Engineering\2025 Spring\MEEN 700\Project\data\radio-localization_2025-04-18-03-08-11.bag" # large_scenario
+# bagfile = r"C:\Users\hanco\OneDrive\Documents\Texas A&M\MS Mechanical Engineering\2025 Spring\MEEN 700\Project\data\radio-localization_2025-04-18-03-08-11.bag" # large_scenario
 # bagfile = r"C:\Users\hanco\OneDrive\Documents\Texas A&M\MS Mechanical Engineering\2025 Spring\MEEN 700\Project\MEEN700-project\radio-localization_2025-04-17-00-14-14.bag"
 
 print("Reading bagfile...")
@@ -49,23 +67,27 @@ rssi_data = b.message_by_topic('/rssi')
 rssi_df = pd.read_csv(rssi_data)[['Time', 'data_0', 'data_1', 'data_2', 'data_3', 'data_4']]
 xy_data = b.message_by_topic("/localization/map_pose")
 xy_df = pd.read_csv(xy_data)[['Time', 'pose.pose.position.x', 'pose.pose.position.y']]
+rssi_df.to_csv('rssi_df.csv', index=False)
+xy_df.to_csv('xy_df.csv', index=False)
 
-print(f'{len(rssi_df)=}')
-print(f'{len(xy_df)=}')
+# print(f'{len(rssi_df)=}')
+# print(f'{len(xy_df)=}')
 
 print("Formatting data...")
 # Merge and time sync the rssi and gps data
 merged_data = pd.merge_asof(rssi_df, xy_df, on="Time", direction='nearest',tolerance=0.01)
-print(f'{len(merged_data)=}')
+# print(f'{len(merged_data)=}')
 for ii, radio in enumerate(config_data['markers']):
     merged_data[f'distance_from_{radio['id']}'] = merged_data.apply(lambda row: calculate_distance(row['pose.pose.position.x'], row['pose.pose.position.y'], [baseStations[0,ii], baseStations[1,ii]]), axis = 1)
 merged_data['rssi'] = merged_data.apply(lambda row: (row['data_1'] + row['data_2'] + row['data_3'] + row['data_4'])/4, axis = 1)
 
-
 merged_data.to_csv('merged_data.csv', index=False)
 
-merged_data = merged_data.bfill()
-merged_data = merged_data.ffill()
+# merged_data = merged_data.bfill()
+# merged_data = merged_data.ffill()
+print(f'{len(merged_data)}')
+merged_data = merged_data.dropna()
+print(f'{len(merged_data)} after dropping na')
 distance_data = merged_data[['distance_from_41341', 'distance_from_41628', 'distance_from_41602', 'data_0', 'rssi']]
 radio_41341_df = distance_data.loc[distance_data['data_0'] == 41341]
 radio_41628_df = distance_data.loc[distance_data['data_0'] == 41628]
@@ -73,9 +95,9 @@ radio_41602_df = distance_data.loc[distance_data['data_0'] == 41602]
 # print(radio_41341_df.head())
 # print(radio_41628_df.head())
 # print(radio_41602_df.head())
-print(f'{len(radio_41341_df)=}')
-print(f'{len(radio_41628_df)=}')
-print(f'{len(radio_41602_df)=}')
+# print(f'{len(radio_41341_df)=}')
+# print(f'{len(radio_41628_df)=}')
+# print(f'{len(radio_41602_df)=}')
 
 
 print("Plotting...")
@@ -93,23 +115,7 @@ for ii, radio in enumerate(config_data['markers']):
 plt.grid()
 plt.axis('equal')
 
-'''
-# Plot rssi over time
-plt.figure()
-plt.title("RSSI vs Time")
-plt.plot(rssi_df.loc[rssi_df['data_0'] == 41602]['Time'], rssi_df.loc[rssi_df['data_0'] == 41602]['data_1'])
-plt.plot(rssi_df.loc[rssi_df['data_0'] == 41628]['Time'], rssi_df.loc[rssi_df['data_0'] == 41628]['data_1'])
-plt.plot(rssi_df.loc[rssi_df['data_0'] == 41341]['Time'], rssi_df.loc[rssi_df['data_0'] == 41341]['data_1'])
-plt.show()
-
-# Plot distance over time
-plt.figure()
-plt.title("Distance vs Time")
-plt.plot(merged_data['Time'],merged_data['distance_from_41602'])
-plt.show()
-'''
-
-#  %% Plot rssi over distance
+# Plot rssi over distance
 plt.figure(figsize=(12, 7))
 # plt.plot(merged_data.iloc[start_ndx[0]:].loc[merged_data['data_0'] == 41602]['distance'].loc[(merged_data['distance'] > 25) & (merged_data['distance'] < 400)], merged_data.iloc[start_ndx[0]:].loc[merged_data['data_0'] == 41602]['rssi'].loc[(merged_data['distance'] > 25) & (merged_data['distance'] < 400)], label='Radio 41602')
 # plt.plot(merged_data.iloc[start_ndx[0]:].loc[merged_data['data_0'] == 41628]['distance'], merged_data.iloc[start_ndx[0]:].loc[merged_data['data_0'] == 41628]['rssi'], label='Radio 41628')
@@ -119,14 +125,16 @@ line_colors = ['tab:blue', 'tab:orange', 'tab:green']
 for ii in range(3):
     distance = distance_data.sort_values(by=f'distance_from_{radio_list[ii]}').loc[distance_data['data_0'] == radio_list[ii]][f'distance_from_{radio_list[ii]}']
     rssi = distance_data.sort_values(by=f'distance_from_{radio_list[ii]}').loc[distance_data['data_0'] == radio_list[ii]]['rssi']
-    popt, _ = curve_fit(dist2rssi, distance, rssi, p0 = [-30, 3])
+    popt, pcov = curve_fit(dist2rssi, distance, rssi, p0 = [-30, 3])
     optimized_power, optimized_N = popt
     rssi_hat = dist2rssi(distance, *(popt))
+    meanSquaredError = MSE(rssi_hat,rssi)
     plt.plot(distance, rssi, line_colors[ii], linestyle='-', label=f'Radio {radio_list[ii]}')    
-    plt.plot(distance, rssi_hat, line_colors[ii], linestyle='--', marker='o', ms=5, mec='r', label=f'Fitted Curve (radio {radio_list[ii]}) baseStationTXpwr={optimized_power:.2f}, rssiN={optimized_N:.2f}')
-plt.title("High Power Calibration - Large Scenario, Map Frame")
+    plt.plot(distance, rssi_hat, line_colors[ii], linestyle='--', marker='o', ms=5, mec='r', label=f'Fitted Curve (radio {radio_list[ii]}) A = {optimized_power:.2f}, N = {optimized_N:.2f}, MSE = {meanSquaredError:.2f}')
+    print(f'{radio_list[ii]} MSE: {meanSquaredError=}')
+    # print(f'{pcov=}')
+plt.title(f"High Power Calibration - {scenario} Scenario")
 plt.xlabel("Distance [m]")
 plt.ylabel("RSSI [dB]")
 plt.legend()
 plt.show()
-
